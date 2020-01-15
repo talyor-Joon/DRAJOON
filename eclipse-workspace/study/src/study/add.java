37015d1c790331825ecda33250ac2f7d355a6132
JUNTAE LEE <talyor@JUNTAEui-MacBookPro.local>
2020-01-15 17:12:14 +0900

    test commit

---
+++ eclipse-workspace/study/src/study/add.java
@@ -0,0 +1,16 @@
+package study;
+
+public class add {
+
+	int a;
+	int b;
+	
+	void setValue(int a, int b) {
+		this.a =a;
+		this.b =b;
+	}
+	
+	void calculate() {
+		System.out.println(a+b);
+	}
+}
